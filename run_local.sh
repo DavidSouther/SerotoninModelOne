74a0ce8b5137ef1f67bd5c85b3593492deb2e158
David Souther <davidsouther@gmail.com>
2020-05-02 21:15:05 +0000

    Add script to run local python on linux with venv dependencies.

---
+++ run_local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+if [[ ! -d .venv ]]; then
+	python3 -m venv .venv
+fi
+
+source ./.venv/bin/activate
+
+python3 -m pip check > /dev/null || \
+	python3 -m pip install -r ./requirements.txt
+
+python3 ./TestTwoColumnNetwork.py
+
