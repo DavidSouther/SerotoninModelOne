6ab4a8e4bb7ee0a5a7b613de63d674acde95d4b2
David Souther <davidsouther@gmail.com>
2020-05-07 20:44:48 -0400

    Add flagsfile and flags for all TwoColumn model params.

--- params.py
+++ params.py
@@ -4,68 +4,74 @@ from absl import flags
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_integer("max_time", 100, "Number of miliseconds per phase.")
+flags.DEFINE_integer("maxTime", 100, "Number of miliseconds per phase.")
 flags.DEFINE_float("tau", 0.1, "Milisecond subdivision resolution.")
-flags.DEFINE_integer("population_count", 20, "Number of neurons per population")
+flags.DEFINE_integer("popCount", 20, "Number of neurons per population")
 
-# Because this will often run in Docker, we can either set parameters using the environment, or using flags.
-# Because of the number of parameter flags, it's expected that a --flagfile would be used. This can be
-# generated programmatically across a number of criteria.
+flags.DEFINE_float("pyramidalSelfExcitationWeight", 25, "pyramidalSelfExcitationWeight")
+flags.DEFINE_float("pyramidalToPyramidalWeight", 50, "pyramidalToPyramidalWeight")
+flags.DEFINE_float("pyramidalToPyramidalLikelihood", 0.3, "pyramidalToPyramidalLikelihood")
+flags.DEFINE_float("PyramidalsToFSWeight", 35, "PyramidalsToFSWeight")
+flags.DEFINE_float("FSToPyramidalsWeight", -100, "FSToPyramidalsWeight")
+flags.DEFINE_float("PyramidalsToLTSWeight", 20, "PyramidalsToLTSWeight")
+flags.DEFINE_float("LTStoFSWeight", -50, "LTStoFSWeight")
+flags.DEFINE_float("LTStoPyramidalsWeight", -50, "LTStoPyramidalsWeight")
 
-def get_max_time():
-    try:
-        return int(os.environ["MAX_TIME"])
-    except:
-        return FLAGS.max_time
+flags.DEFINE_float("inputWeightA", 50, "inputWeightA")
+flags.DEFINE_float("inputWeightB", 50, "inputWeightB")
+flags.DEFINE_float("rateA", 1, "rateA")
+flags.DEFINE_float("rateB", 1, "rateB")
+flags.DEFINE_float("inputWeightAB", 25, "inputWeightAB")
+flags.DEFINE_float("crossModalABLikelihood", 0.5, "crossModalABLikelihood")
+flags.DEFINE_float("inputWeightBA", 25, "inputWeightBA")
+flags.DEFINE_float("crossModalBALikelihood", 0.5, "crossModalBALikelihood")
 
-def get_tau():
-    try:
-        return float(os.environ["TAU"])
-    except:
-        return FLAGS.tau
-
-def get_pop_count():
-    try:
-        return int(os.environ["POPULATION_COUNT"])
-    except:
-        return FLAGS.population_count
-
+flags.DEFINE_float("serotoninLevelA", 10, "serotoninLevelA")
+flags.DEFINE_float("serotoninLevelB", 10, "serotoninLevelB")
+flags.DEFINE_float("Somatic5HT2AWeight", 85, "Somatic5HT2AWeight")
+flags.DEFINE_float("Somatic5HT2AWeightLTS", 81, "Somatic5HT2AWeightLTS")
+flags.DEFINE_float("Somatic5HT1AWeight", -80, "Somatic5HT1AWeight")
+flags.DEFINE_float("Axonal5HT2AWeight", 0.4, "Axonal5HT2AWeight")
+flags.DEFINE_float("Axonal5HT1AWeight", -0.4, "Axonal5HT1AWeight")
+    
 def loadParams():
     params = {}
     # Time Parameters
-    params["maxTime"] = get_max_time()
-    params["tau"] = get_tau()
+    params["maxTime"] = FLAGS.maxTime
+    params["tau"] = FLAGS.tau
 
     # Population and Connection Parameters
-    params["popCount"] = get_pop_count()
+    params["popCount"] = FLAGS.popCount
 
-    params["pyramidalSelfExcitationWeight"] = 25
-    params["pyramidalToPyramidalWeight"] = 50
-    params["pyramidalToPyramidalLikelihood"] = 0.3
-    params["PyramidalsToFSWeight"] = 35
-    params["FSToPyramidalsWeight"] = -100
-    params["PyramidalsToLTSWeight"] = 20 #500
-    params["LTStoFSWeight"] = -50
-    params["LTStoPyramidalsWeight"] = -50
+    params["pyramidalSelfExcitationWeight"] = FLAGS.pyramidalSelfExcitationWeight
+    params["pyramidalToPyramidalWeight"] = FLAGS.pyramidalToPyramidalWeight
+    params["pyramidalToPyramidalLikelihood"] = FLAGS.pyramidalToPyramidalLikelihood
+    params["PyramidalsToFSWeight"] = FLAGS.PyramidalsToFSWeight
+    params["FSToPyramidalsWeight"] = FLAGS.FSToPyramidalsWeight
+    params["PyramidalsToLTSWeight"] = FLAGS.PyramidalsToLTSWeight
+    params["LTStoFSWeight"] = FLAGS.LTStoFSWeight
+    params["LTStoPyramidalsWeight"] = FLAGS.LTStoPyramidalsWeight
 
     ## Regular
     # Input Paramters
-    params["inputWeightA"] = 50
-    params["inputWeightB"] = 50
-    params["rateA"] = 1
-    params["rateB"] = 1
-    params["inputWeightAB"] = 25
-    params["crossModalABLikelihood"] = 0.5
-    params["inputWeightBA"] = 25 #7500
-    params["crossModalBALikelihood"] = 0.5
+    params["inputWeightA"] = FLAGS.inputWeightA
+    params["inputWeightB"] = FLAGS.inputWeightB
+    params["rateA"] = FLAGS.rateA
+    params["rateB"] = FLAGS.rateB
+    params["inputWeightAB"] = FLAGS.inputWeightAB
+    params["crossModalABLikelihood"] = FLAGS.crossModalABLikelihood
+    params["inputWeightBA"] = FLAGS.inputWeightBA
+    params["crossModalBALikelihood"] = FLAGS.crossModalBALikelihood
 
     # Diffuse Neurotransmitter Paramters
-    params["serotoninLevelA"] = 10
-    params["serotoninLevelB"] = 10
-    params["Somatic5HT2AWeight"] = 85
-    params["Somatic5HT2AWeightLTS"] = 81
-    params["Somatic5HT1AWeight"] = -80
-    params["Axonal5HT2AWeight"] = 0.4
-    params["Axonal5HT1AWeight"] = -0.4
+    params["serotoninLevelA"] = FLAGS.serotoninLevelA
+    params["serotoninLevelB"] = FLAGS.serotoninLevelB
+    params["Somatic5HT2AWeight"] = FLAGS.Somatic5HT2AWeight
+    params["Somatic5HT2AWeightLTS"] = FLAGS.Somatic5HT2AWeightLTS
+    params["Somatic5HT1AWeight"] = FLAGS.Somatic5HT1AWeight
+    params["Axonal5HT2AWeight"] = FLAGS.Axonal5HT2AWeight
+    params["Axonal5HT1AWeight"] = FLAGS.Axonal5HT1AWeight
+
+    print(params)
 
     return params
